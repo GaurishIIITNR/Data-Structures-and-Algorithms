b7bdb9e8ed69e32f6e1b49a34eabc9f57c08ff93
Gaurish Ojha <gaurish21102@iiitnr.edu.in>
2022-10-05 16:59:09 +0530

    TEST COMMIT\n helllo hello\nkkkk

--- CPP/array-2d/all_pairs.cpp
+++ CPP/array-2d/all_pairs.cpp
@@ -1,25 +1,27 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int all_pair(int arr[], int n){
+int all_pair(int arr[], int n)
+{
 	for (int i = 0; i < n; ++i)
 	{
-		for (int j=i+1 ; j < n; ++j)
+		for (int j = i + 1; j < n; ++j)
 		{
-			cout<<arr[i]<<", "<<arr[j]<<endl;
+			cout << arr[i] << ", " << arr[j] << endl;
 		}
 	}
 	return 0;
 }
-
-int main(){
-		int n;
-		cin>>n;
-		int arr[n];
-		for (int i = 0; i < n; ++i)
-		{
-			cin>>arr[i];
-		}
-		all_pair(arr, n);
+// TEST COMMIT.
+int main()
+{
+	int n;
+	cin >> n;
+	int arr[n];
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> arr[i];
+	}
+	all_pair(arr, n);
 
 	return 0;
 }
